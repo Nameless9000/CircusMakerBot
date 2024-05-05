79c9f8860c5c9d5d25d75bc6e5e376238408ed73
Nameless 9000 <dev@name1ess.ga>
2024-05-05 21:22:47 +0100

    Working bot

---
+++ DiscordBot/Commands/Ping.cs
@@ -0,0 +1,7 @@
+﻿using DSharpPlus.Commands;
+
+public class PingCommand
+{
+    [Command("ping")]
+    public static ValueTask ExecuteAsync(CommandContext context) => context.RespondAsync($"Pong! Latency is {context.Client.Ping}ms.");
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,98 +1,73 @@
-﻿using System;
-using System.Linq;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.Interactivity;
-using DSharpPlus.Interactivity.Extensions;
+﻿using DSharpPlus;
+using DSharpPlus.Commands;
 using Microsoft.Extensions.Configuration;
-using System.Windows.Input;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using DSharpPlus.Commands.Processors.TextCommands;
+using DSharpPlus.Commands.Processors.TextCommands.Parsing;
 
 namespace DiscordBot
 {
     class Program
     {
-        /* This is the cancellation token we'll use to end the bot if needed(used for most async stuff). */
-        private CancellationTokenSource? cts { get; set; }
-
-        /* We'll load the app config into this when we create it a little later. */
-        private IConfigurationRoot? config;
-
-        /* These are the discord library's main classes */
-        private DiscordClient? discord;
-        private CommandsNextExtension? commands;
-        private InteractivityExtension? interactivity;
-
-        static async Task Main(string[] args) => await new Program().InitBot(args);
-
-        async Task InitBot(string[] args)
+        [RequiresUnreferencedCode("Calls Microsoft.Extensions.Configuration.ConfigurationBinder.GetValue<T>(String)")]
+        static async Task Main(string[] args)
         {
-            try
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config/config.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            string? command_prefix = config.GetValue<string>("discord:CommandPrefix");
+            if (command_prefix == null)
+                throw new ArgumentNullException(nameof(command_prefix));
+
+            string? token = config.GetValue<string>("discord:token");
+            if (string.IsNullOrWhiteSpace(token))
             {
-                Console.WriteLine("[info] Welcome to my bot!");
-                cts = new CancellationTokenSource();
-
-                // Load the config file(we'll create this shortly)
-                Console.WriteLine("[info] Loading config file..");
-                config = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("config.json", optional: false, reloadOnChange: true)
-                    .Build();
-
-                // Create the DSharpPlus client
-                Console.WriteLine("[info] Creating discord client..");
-                discord = new DiscordClient(new DiscordConfiguration
-                {
-                    Token = config.GetValue<string>("discord:token"),
-                    TokenType = TokenType.Bot
-                });
-
-                // Create the interactivity module(I'll show you how to use this later on)
-                interactivity = discord.UseInteractivity(new InteractivityConfiguration()
-                {
-                    Timeout = TimeSpan.FromSeconds(30) // Default time to wait for interactive commands like waiting for a message or a reaction
-                });
-
-                // Build dependancies and then create the commands module.
-                commands = discord.UseCommandsNext(new CommandsNextConfiguration
-                {
-                    StringPrefixes = [config.GetValue<string>("discord:CommandPrefix")], // Load the command prefix(what comes before the command, eg "!" or "/") from our config file
-                });
-
-                // Add command loading
-                Console.WriteLine("[info] Loading command modules..");
-
-                var type = typeof(IModule); // Get the type of our interface
-                var types = AppDomain.CurrentDomain.GetAssemblies() // Get the assemblies associated with our project
-                    .SelectMany(s => s.GetTypes()) // Get all the types
-                    .Where(p => type.IsAssignableFrom(p) && !p.IsInterface); // Filter to find any type that can be assigned to an IModule
-
-                var typeList = types as Type[] ?? types.ToArray(); // Convert to an array
-                foreach (var t in typeList)
-                    commands.RegisterCommands(t); // Loop through the list and register each command module with CommandsNext
-
-                Console.WriteLine($"[info] Loaded {typeList.Count()} modules.");
-
-                RunAsync(args).Wait();
+                Console.WriteLine("Error: No discord token found. Please provide a token via the DISCORD_TOKEN environment variable.");
+                Environment.Exit(1);
             }
-            catch (Exception ex)
+
+            DiscordShardedClient discord = new (new DiscordConfiguration
             {
-                Console.Error.WriteLine(ex.ToString());
+                Token = token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All
+            });
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton(discord);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Use the commands extension
+            IReadOnlyDictionary<int, CommandsExtension> commandsExtensions = await discord.UseCommandsAsync(new CommandsConfiguration()
+            {
+                ServiceProvider = serviceProvider,
+                DebugGuildId = 1235311014342295595,
+                RegisterDefaultCommandProcessors = true
+            });
+
+            // Iterate through each Discord shard
+            foreach (CommandsExtension commandsExtension in commandsExtensions.Values)
+            {
+                // Add all commands by scanning the current assembly
+                commandsExtension.AddCommands(typeof(Program).Assembly);
+                TextCommandProcessor textCommandProcessor = new(new()
+                {
+                    // By default, the prefix will be "!"
+                    // However the bot will *always* respond to a direct mention
+                    // as long as the `DefaultPrefixResolver` is used
+                    PrefixResolver = new DefaultPrefixResolver(command_prefix).ResolvePrefixAsync
+                });
+
+                // Add text commands with a custom prefix (?ping)
+                await commandsExtension.AddProcessorsAsync(textCommandProcessor);
             }
-        }
 
-        async Task RunAsync(string[] args)
-        {
-            // Connect to discord's service
-            Console.WriteLine("Connecting..");
-            await discord.ConnectAsync();
-            Console.WriteLine("Connected!");
-
-            // Keep the bot running until the cancellation token requests we stop
-            while (!cts.IsCancellationRequested)
-                await Task.Delay(TimeSpan.FromMinutes(1));
+            await discord.StartAsync();
+            await Task.Delay(-1);
         }
     }
 }
