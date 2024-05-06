b5ac61f709295f2a0d984f27b5b90e24ed9ec15d
Nameless 9000 <dev@name1ess.ga>
2024-05-06 02:08:37 +0100

    Calculator eval

---
+++ Calculator/Library.fs
@@ -0,0 +1,61 @@
+﻿namespace Math
+
+open System.Text.RegularExpressions
+
+module Calculator =
+    type BinOp = Add | Sub | Mul | Div | Rem | Exp | Xor | Or | And
+
+    type Expr =
+        | Number of float
+        | BinaryOp of BinOp * Expr * Expr
+
+    let rec eval = function
+        | Number n -> n
+        | BinaryOp (op, e1, e2) ->
+            let f1 = eval e1 in
+            let f2 = eval e2 in
+            let i1 = int (System.Math.Round(f1)) in
+            let i2 = int (System.Math.Round(f2)) in
+
+            match op with
+            | Add -> f1 + f2
+            | Sub -> f1 - f2
+            | Mul -> f1 * f2
+            | Div -> f1 / f2
+            | Rem -> f1 % f2
+            | Exp -> f1 ** f2
+            | Xor -> int (i1 ^^^ i2)
+            | Or -> int (i1 ||| i2)
+            | And -> int (i1 &&& i2)
+
+    let rec parse_expr (tokens: list<string>) =
+        let rec inner (acc: Expr) (tokens: list<string>) =
+            match tokens with
+            | [] -> acc
+            | num :: _ when System.Double.TryParse(num).Equals(true) ->
+                Number(float num)
+            | symbol :: tail ->
+                let op =
+                    match symbol with
+                    | "+" -> Add
+                    | "-" -> Sub
+                    | "*" -> Mul
+                    | "/" -> Div
+                    | "%" -> Rem
+                    | "**" -> Exp
+                    | "^" -> Xor
+                    | "|" -> Or
+                    | "&" -> And
+                    | _ -> failwith "Unknown operator"
+
+                BinaryOp(op, acc, parse_expr tail)
+
+        if tokens.Length = 0 then
+            failwith "Unexpected end of input"
+
+        inner (Number(float (List.head tokens))) (List.tail tokens)
+
+
+    let tokenize (input: string) =
+        let matches = Regex.Matches(input, "((?<=^|[+\-*/^|&]\*?)[+-]?([0-9]*[.])?[0-9]+)|([+\-*/^|&]\*?)")
+        [ for m in matches -> m.Value ]
--- DiscordBot/Commands/Math.cs
+++ DiscordBot/Commands/Math.cs
@@ -1,12 +1,38 @@
 ﻿using DSharpPlus.Commands;
 using System.ComponentModel;
 
+namespace Math;
+
 [Command("math"), Description("Group of math related commands")]
 public class MathCommands
 {
     [Command("add"), Description("Add two numbers")]
-    public static ValueTask AddAsync(CommandContext context, int a, int b) => context.RespondAsync($"{a} + {b} = {a + b}");
+    public static ValueTask AddAsync(CommandContext context, double a, double b) => context.RespondAsync($"{a} + {b} = {a + b}");
 
     [Command("sub"), Description("Subtract two numbers")]
-    public static ValueTask SubtractAsync(CommandContext context, int a, int b) => context.RespondAsync($"{a} - {b} = {a - b}");
+    public static ValueTask SubtractAsync(CommandContext context, double a, double b) => context.RespondAsync($"{a} - {b} = {a - b}");
+
+    [Command("eval"), Description("Evaluates an expression")]
+    public static ValueTask EvalAsync(CommandContext context, params string[] input)
+    { 
+        try
+        {
+            var processed_input = string.Join(" ", input)
+                .Trim()
+                .Replace(" ", "");
+
+            var tokens = Calculator.tokenize(processed_input);
+            if (tokens.Length == 0)
+                return context.RespondAsync("Error: No tokens found");
+
+            var ast = Calculator.parse_expr(tokens);
+
+            var result = Calculator.eval(ast);
+
+            return context.RespondAsync("Result: " + result.ToString());
+        } catch (Exception ex)
+        {
+            return context.RespondAsync("Error: " + ex.Message);
+        }
+    }
 }
